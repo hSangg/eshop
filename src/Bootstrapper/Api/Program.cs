feba7b90f5efe140848e46b68fc5b79607887f39
hoai sang <hoai.sang050@gmail.com>
2025-01-13 21:45:39 +0700

    Add CatalogModule.cs

---
+++ src/Bootstrapper/Api/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services.
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.Run();
