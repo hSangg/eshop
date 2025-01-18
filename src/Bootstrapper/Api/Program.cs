9bd0a49d59d976a0164f97c76008af3662db96ec
hoai sang <hoai.sang050@gmail.com>
2025-01-18 09:28:12 +0700

    Develop shared domain for Tactical DDD

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -14,5 +14,4 @@ app
     .UseBasketModule()
     .UseOrderingModule();
 
-
 app.Run();
---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Shared.DDD;
+
+public interface IDomainEvent : INotification
+{
+    Guid EventId { get; set; }
+    public DateTime OccurredOn { get; set; }
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
+
